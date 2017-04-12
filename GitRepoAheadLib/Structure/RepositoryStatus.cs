3f6a6af5b1f15711d85a957bdce8b076d79e0481
shane <shane.powell@tftp.biz>
2017-04-12 14:42:42 +0100

    Added RepositoryStatus.cs structure. Added functions to make use of new structure as an alternative to string responses.

---
+++ GitRepoAheadLib/Structure/RepositoryStatus.cs
@@ -0,0 +1,80 @@
+﻿namespace GitRepoAheadLib.Structure
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class RepositoryStatus : INotifyPropertyChanged
+    {
+        private string name = string.Empty;
+
+        private bool ahead = false;
+
+        private bool behind = false;
+
+        private string path = string.Empty;
+
+        public RepositoryStatus(string nameIn, string pathIn)
+        {
+            this.name = nameIn;
+            this.path = pathIn;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public bool Ahead
+        {
+            get
+            {
+                return this.ahead;
+            }
+            set
+            {
+                this.ahead = value;
+                this.OnPropertyChanged();
+            }
+        }
+        // Not yet implemented.
+        //public bool Behind
+        //{
+        //    get
+        //    {
+        //        return this.behind;
+        //    }
+        //    set
+        //    {
+        //        this.behind = value;
+        //        this.OnPropertyChanged();
+        //    }
+        //}
+
+        public string Path
+        {
+            get
+            {
+                return this.path;
+            }
+            set
+            {
+                this.path = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ GitRepoAheadLib/Utilities/GitRepoTools.cs
@@ -0,0 +1,155 @@
+﻿namespace GitRepoAheadLib.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Runtime.CompilerServices;
+
+    using GitRepoAheadLib.Structure;
+
+    public class GitRepoTools
+    {
+        /// <summary>
+        /// Looks for git repositories within the root folder and checks to see if they are ahead of the remote repository.
+        /// </summary>
+        /// <param name="rootFolder">
+        /// The root folder.
+        /// </param>
+        /// <returns>
+        /// The list or repositories that are ahead <see cref="List"/>.
+        /// </returns>
+        public static List<string> GetAheadReposFromRootDir(string rootFolder)
+        {
+            var unpushedRepos = new List<string>();
+
+            if (Directory.Exists(rootFolder))
+            {
+                var repos = Directory.GetDirectories(rootFolder);
+
+                foreach (var repo in repos)
+                {
+                    if (!Directory.Exists($"{repo}\\.git"))
+                    {
+                        continue;
+                    }
+
+                    if (CheckIfRepoIsAhead(repo))
+                    {
+                        // Remove path from repo name
+                        var repoName = GetRepoName(rootFolder, repo);
+                        unpushedRepos.Add(repoName);
+                    }
+                }
+            }
+
+            return unpushedRepos;
+        }
+
+        private static bool CheckIfRepoIsAhead(string repoPath)
+        {
+            var ahead = false;
+
+            // Setup git command
+            ProcessStartInfo startInfo =
+                new ProcessStartInfo("cmd", "/c " + $"git -C {repoPath} branch -v")
+                    {
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    };
+
+            Process process = Process.Start(startInfo);
+
+            if (process == null)
+            {
+                return false;
+            }
+
+            // Create handler for output event
+            process.OutputDataReceived += (s, e) =>
+                {
+                    if (e.Data != null)
+                    {
+                        // Check for ahead in string to see if ahead of remote.
+                        if (e.Data.Contains("ahead"))
+                        {
+                            // Flag unpushed changes
+                            ahead = true;
+                        }
+                    }
+                };
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+
+            return ahead;
+        }
+
+        private static string GetRepoName(string rootFolder, string repo)
+        {
+            var repoName = repo.Replace(rootFolder, string.Empty);
+            repoName = repoName.Replace(@"\", string.Empty);
+            return repoName;
+        }
+
+        public static List<string> GetGitReposInDirString(string rootFolder)
+        {
+            var detectedRepos = new List<string>();
+
+            if (Directory.Exists(rootFolder))
+            {
+                var repos = Directory.GetDirectories(rootFolder);
+
+                foreach (var repo in repos)
+                {
+                    if (Directory.Exists($"{repo}\\.git"))
+                    {
+                        var repoName = GetRepoName(rootFolder, repo);
+                        detectedRepos.Add(repoName);
+                    }
+                }
+            }
+
+            return detectedRepos;
+        }
+
+        public static List<RepositoryStatus> GetGitReposInDir(string rootFolder)
+        {
+            var detectedRepos = new List<RepositoryStatus>();
+
+            if (Directory.Exists(rootFolder))
+            {
+                var repos = Directory.GetDirectories(rootFolder);
+
+                foreach (var repo in repos)
+                {
+                    var path = $"{repo}\\.git";
+
+                    if (Directory.Exists(path))
+                    {
+                        var repoName = GetRepoName(rootFolder, repo);
+
+                        var repoStatus = new RepositoryStatus(repoName, path);
+
+                        detectedRepos.Add(repoStatus);
+                    }
+                }
+            }
+
+            return detectedRepos;
+        }
+
+        public static List<RepositoryStatus> GetAllReposAndStatusFromRootDir(string rootFolder)
+        {
+            var repos = GetGitReposInDir(rootFolder);
+
+            foreach (var repositoryStatus in repos)
+            {
+                repositoryStatus.Ahead = CheckIfRepoIsAhead(repositoryStatus.Path);
+            }
+
+            return repos;
+        }
+    }
+}
